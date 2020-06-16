b6ce35dcb4994da8ead703ec97f7eab7049d11e0
gutovesco <guto.vesco@hotmail.com>
2020-06-16 09:32:40 -0300

    finished module

--- src/modules/appointments/services/ListProviderAppointmentsService.ts
+++ src/modules/appointments/services/ListProviderAppointmentsService.ts
@@ -1,3 +1,4 @@
+import { classToClass } from 'class-transformer';
 import { inject, injectable } from 'tsyringe';
 
 import ICacheProvider from '@shared/container/providers/CacheProvider/models/ICacheProvider';
@@ -44,7 +45,7 @@ class ListProviderAppointmentsService {
       );
       console.log('Buscou');
 
-      await this.cacheProvider.save(cacheKey, appointments);
+      await this.cacheProvider.save(cacheKey, classToClass(appointments));
     }
 
     return appointments;
